fb28d38b7d5e699e264e728def1e2f69da7b2254
Taugenichts89 <124059463+Taugenichts89@users.noreply.github.com>
2023-02-02 11:10:01 +0100

    firstDraft

---
+++ index.js
@@ -0,0 +1,27 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let firstPw = document.getElementById('first-pw')
+let secondPw = document.getElementById('second-pw')
+
+
+function displayPasswords(){
+    firstPw.textContent = generatePassword()
+    secondPw.textContent = generatePassword()
+}
+
+function generatePassword(){
+    let password =""
+    for(let i = 0; i<15; i++){
+        password += characters[Math.floor(Math.random()*characters.length+1)]
+
+    }
+    return password
+}
+
+function copyToClipboard(str){
+    
+    navigator.clipboard.writeText(document.getElementById(str).textContent)
+    alert("Password copied to clipboard!")
+
+}
